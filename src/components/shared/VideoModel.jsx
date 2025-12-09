8de43f7a3898687feafa72980c87419129413d1d
Arman Mia <mdarman365506@gmail.com>
2025-12-09 22:48:30 +0600

    feat(video): implement video modal in hero section

--- src/components/shared/VideoModel.jsx
+++ src/components/shared/VideoModel.jsx
@@ -1,7 +1,19 @@
 const VideoModal = ({ onClose }) => {
+  const handleBackdropClick = (e) => {
+
+    if (e.target.id === "video-backdrop") {
+      onClose();
+    }
+  };
+
   return (
-    <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50">
+    <div
+      id="video-backdrop"
+      className="fixed inset-0 bg-black/70 flex items-center justify-center z-50"
+      onClick={handleBackdropClick}
+    >
       <div className="relative bg-black rounded-xl overflow-hidden max-w-6xl w-full">
+        {/* Video iframe */}
         <div className="relative pb-[56.25%] h-0">
           <iframe
             className="absolute top-0 left-0 w-full h-full rounded-xl"
@@ -13,6 +25,7 @@ const VideoModal = ({ onClose }) => {
           ></iframe>
         </div>
 
+        {/* Close button */}
         <button
           onClick={onClose}
           className="absolute top-2 right-2 text-2xl text-white cursor-pointer bg-red-500 hover:bg-red-600 pl-2 pr-3 py-1 rounded-full"
